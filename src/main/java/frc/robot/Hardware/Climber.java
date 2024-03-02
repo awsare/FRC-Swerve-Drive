754614b6ea1021125b830931fa2237cd804a1ee9
awsare <99767480+awsare@users.noreply.github.com>
2024-03-02 15:21:55 -0500

    everything is updated

--- src/main/java/frc/robot/Hardware/Climber.java
+++ src/main/java/frc/robot/Hardware/Climber.java
@@ -1,23 +1,22 @@
 package frc.robot.Hardware;
 
-import com.ctre.phoenix6.controls.DutyCycleOut;
+
 
 // import com.ctre.phoenix.motorcontrol.ControlMode;
 // import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 
 public class Climber {
     //private final TalonFX kraken;
     private final TalonFX kraken;
-    private final DutyCycleOut out = new DutyCycleOut(0);
 
     public Climber() {
         kraken = new TalonFX(9);
     }
 
     public void setClimberSpeed(double speed) {
-        out.Output = speed;
-        kraken.setControl(out);
+        kraken.set(speed);
     }
 }
--- src/main/java/frc/robot/Hardware/SwerveModule.java
+++ src/main/java/frc/robot/Hardware/SwerveModule.java
@@ -1,18 +1,20 @@
 package frc.robot.Hardware;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix6.hardware.CANcoder;
+
+// import com.ctre.phoenix.motorcontrol.ControlMode;
+// import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.controller.PIDController;
-
-import com.ctre.phoenix.sensors.WPI_CANCoder;
 
 public class SwerveModule {
     private final TalonFX steerMotor;
     private final TalonFX driveMotor;
 
     private final PIDController steerController;
-    private final WPI_CANCoder encoder;
+    private final CANcoder encoder;
 
     private final double KP;
     private final double KI;
@@ -28,9 +30,9 @@ public class SwerveModule {
 
         steerController = new PIDController(KP, KI, KD);
         steerController.enableContinuousInput(-180, 180);
-        encoder = new WPI_CANCoder(ENCODERID);
-        encoder.configMagnetOffset(OFFSET);
-        encoder.setPositionToAbsolute();
+        encoder = new CANcoder(ENCODERID);
+        // encoder.configMagnetOffset(OFFSET);
+        // encoder.setPositionToAbsolute();
     }
 
     public void setConstants(double KP, double KI, double KD) {
@@ -40,11 +42,11 @@ public class SwerveModule {
     }
 
     public void setSteerAngle(double angle) {        
-        double output = steerController.calculate(encoder.getPosition(), angle);
-        steerMotor.set(ControlMode.PercentOutput, output);
+        double output = steerController.calculate(encoder.getPosition().getValue(), angle);
+        steerMotor.set(output);
     }
 
     public void setDriveSpeed(double speed) {
-        driveMotor.set(ControlMode.PercentOutput, speed);
+        driveMotor.set(speed);
     }
 }
