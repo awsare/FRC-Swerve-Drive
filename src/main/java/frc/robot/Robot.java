9f8de3e518c9aab051a75f1bbb226b9996c92924
Neel Awsare <99767480+awsare@users.noreply.github.com>
2024-04-05 20:33:53 +0200

    Acceleration curve

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,17 +2,12 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import frc.robot.Hardware.Climber;
-import frc.robot.Hardware.Shooter;
 import frc.robot.Hardware.SwerveChassis;
 
 public class Robot extends TimedRobot {
     Joystick driver = new Joystick(0);
-    Joystick operator = new Joystick(1);
-
-    //SwerveChassis swerveChassis = new SwerveChassis();
-    //Shooter shooter = new Shooter();
-    Climber climber = new Climber();
+    
+    SwerveChassis swerveChassis = new SwerveChassis();
 
     @Override
     public void robotInit() {}
@@ -34,9 +29,12 @@ public class Robot extends TimedRobot {
         double x = -driver.getRawAxis(0);
         double y = -driver.getRawAxis(1);
         double r = -driver.getRawAxis(4);
-        //swerveChassis.drive(x, y, r);
 
-        double climberSpeed = driver.getRawAxis(3) - driver.getRawAxis(2);
-        climber.setClimberSpeed(climberSpeed);
+        double a = 0.6;
+        x = (a * Math.pow(x, 3)) + (x * (1 - a));
+        y = (a * Math.pow(y, 3)) + (y * (1 - a));
+        r = (a * Math.pow(r, 3)) + (r * (1 - a));
+        
+        swerveChassis.drive(x, y, r);
     }
 }
